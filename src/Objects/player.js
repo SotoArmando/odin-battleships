8eed4e94da3cfb08c2f281d27c0fabc717e6b23f
armando2929 <armando@dix.do>
2020-03-02 21:36:53 -0700

    added + fixes latest review

--- src/Objects/player.js
+++ src/Objects/player.js
@@ -4,100 +4,100 @@ import ship from './ship';
 
 
 export default function player(
-	index,
-	data = [...(' '.repeat(100).split('').map(() => false))], strikes = [...(' '.repeat(100).split('').map(() => false))], playerrender = true,
+  index,
+  data = [...(' '.repeat(100).split('').map(() => false))], strikes = [...(' '.repeat(100).split('').map(() => false))], playerrender = true,
 ) {
-	return {
-		data: [...(' '.repeat(100).split('').map(() => false))],
-		strikes: [...(' '.repeat(100).split('').map(() => false))],
-		index,
-		renderer: render(),
-		initallevents: (board) => {
-			this.renderer.initEventListeners(board);
-		},
-		renderreset: () => {
-			this.renderer.reset();
-		},
-		reset() {
-			const newdata = [...(' '.repeat(100).split('').map(() => false))];
-			this.data = newdata;
-			this.strikes = newdata;
-			const a = this.data;
-			const b = this.index;
-			this.chips = [2, 3, 4, 4, 2, 2, 3, 6].map((size) => {
-				let i = 0;
-				let thisship = ship(size, 0, 0);
-				while (i < 5000) {
-					i += 1;
-					const position = Math.floor(Math.random() * 99);
-					const direction = Math.floor(Math.random() * 4);
-					const condition = positionCalculator(0, data)
-						.isSomethingThere({position, direction}, size);
-					if (!condition) {
-						thisship = ship({
-							size,
-							position,
-							direction,
-						});
-						break;
-					}
-				}
-				render().addShip(b, positionCalculator(0, a)
-					.allpositions(thisship.vertex, thisship.size));
-				return thisship;
-			});
-		},
-		initPlayer: () => {
-			this.renderer.renderLabels();
-		},
-		chips: (() => {
-			[2, 3, 4, 4, 2, 2, 3, 6].map((size, index) => {
-				let thisship = ship(size, 0, 0);
-				let i = 0;
-				if (playerrender) {
-					while (i < 5000) {
-						i += 1;
-						const position = Math.floor(Math.random() * 99);
-						const direction = Math.floor(Math.random() * 4);
-						const condition = positionCalculator(0, data)
-						.isSomethingThere({position, direction}, size);
-						if (!condition)
-						{
-							thisship = ship({
-								size,
-								position,
-								direction,
-							});
-							break;
-						} 
-					}
-					render().addShip(index, positionCalculator(0, data)
-						.allpositions(thisship.vertex, thisship.size));
-				}
-				else 
-				{
-					thisship = ship({
-						size,
-						index,
-						direction: 0,
-					});
-				}
-				return thisship;
-			});
-		})(),
-		strike: (position) => {
-			if (!this.renderer) {
-				this.renderer = render();
-			}
-			if (!this.data) {
-				this.data = data;
-			}
-			if (!this.strikes) {
-				this.strikes = strikes;
-			}
-			this.data[position] = true;
-			this.renderer
-				.strikePosition(this.index, position, this.data[position]);
-		},
-	};
+  return {
+    data: [...(' '.repeat(100).split('').map(() => false))],
+    strikes: [...(' '.repeat(100).split('').map(() => false))],
+    index,
+    renderer: render(),
+    initallevents: (board) => {
+      this.renderer.initEventListeners(board);
+    },
+    renderreset: () => {
+      this.renderer.reset();
+    },
+    reset() {
+      const newdata = [...(' '.repeat(100).split('').map(() => false))];
+      this.data = newdata;
+      this.strikes = newdata;
+      const a = this.data;
+      const b = this.index;
+      this.chips = [2, 3, 4, 4, 2, 2, 3, 6].map((size) => {
+        let i = 0;
+        let thisship = ship(size, 0, 0);
+        while (i < 5000) {
+          i += 1;
+          const position = Math.floor(Math.random() * 99);
+          const direction = Math.floor(Math.random() * 4);
+          const condition = positionCalculator(0, data)
+            .isSomethingThere({position, direction}, size);
+          if (!condition) {
+            thisship = ship({
+              size,
+              position,
+              direction,
+            });
+            break;
+          }
+        }
+        render().addShip(b, positionCalculator(0, a)
+          .allpositions(thisship.vertex, thisship.size));
+        return thisship;
+      });
+    },
+    initPlayer: () => {
+      this.renderer.renderLabels();
+    },
+    chips: (() => {
+      [2, 3, 4, 4, 2, 2, 3, 6].map((size, index) => {
+        let thisship = ship(size, 0, 0);
+        let i = 0;
+        if (playerrender) {
+          while (i < 5000) {
+            i += 1;
+            const position = Math.floor(Math.random() * 99);
+            const direction = Math.floor(Math.random() * 4);
+            const condition = positionCalculator(0, data)
+            .isSomethingThere({position, direction}, size);
+            if (!condition)
+            {
+              thisship = ship({
+                size,
+                position,
+                direction,
+              });
+              break;
+            } 
+          }
+          render().addShip(index, positionCalculator(0, data)
+            .allpositions(thisship.vertex, thisship.size));
+        }
+        else 
+        {
+          thisship = ship({
+            size,
+            index,
+            direction: 0,
+          });
+        }
+        return thisship;
+      });
+    })(),
+    strike: (position) => {
+      if (!this.renderer) {
+        this.renderer = render();
+      }
+      if (!this.data) {
+        this.data = data;
+      }
+      if (!this.strikes) {
+        this.strikes = strikes;
+      }
+      this.data[position] = true;
+      this.renderer
+        .strikePosition(this.index, position, this.data[position]);
+    },
+  };
 }
