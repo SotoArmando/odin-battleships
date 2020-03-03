6cc37536d9b76a5377940e3dd5246074f76b7bbb
armando2929 <armando@dix.do>
2020-03-02 20:50:56 -0700

    Stickler errors fixes

--- src/Objects/domrenderer.js
+++ src/Objects/domrenderer.js
@@ -1,12 +1,9 @@
-import { positionCalculator } from './positionCalculator';
+
 
 export default function render() {
-  let initialized = false;
 
   return {
-    addShip: (playerindex, allpositions, vertex, size) => {
-  
-
+    addShip: (playerindex, allpositions) => {
       const color = `rgba(${(Math.random() * 255)},${(Math.random() * 255)},${(Math.random() * 255)},0.3)`;
       allpositions.forEach((position) => {
         document.querySelector(`cells[data-playerid='${playerindex}'] > item[data-id='${position}']`).classList.add('active');
@@ -17,7 +14,7 @@ export default function render() {
       return allpositions;
     },
     initEventListeners: (board) => {
-      document.querySelectorAll(`cells[data-playerid='${1}'] > item`).forEach(x => x.addEventListener('click', function abc(listener) {
+      document.querySelectorAll(`cells[data-playerid='${1}'] > item`).forEach(x => x.addEventListener('click', function abc() {
         board.rollTurns(this.getAttribute('data-id'));
         board.rollTurns(Math.floor(Math.random() * 99));
       }));
@@ -34,13 +31,11 @@ export default function render() {
       document.querySelector('#span_player1_label').innerHTML = '<span style="font-weight: 600;">Board - Player 1</span><br />Its your turn, play!';
       document.querySelector('#span_player2_label').innerHTML = '<span style="font-weight: 600;">Board - CPU</span><br />Auto player';
     },
-    strikePosition: (playerindex, position, done) => {
-
-
+    strikePosition: (playerindex, position) => {
       if (document.querySelector(`cells[data-playerid='${playerindex}'] > item[data-id='${position}']`).classList.contains('active')) {
         document.querySelector(`cells[data-playerid='${playerindex}'] > item[data-id='${position}']`).classList.add('striked');
         const otherplayer = (playerindex === 1) ? 0 : 1;
-        document.querySelector(`#span_score[data-playerid='${playerindex}']`).innerHTML = parseInt(document.querySelector(`#span_score[data-playerid='${playerindex}']`).innerHTML,10) - 1;
+        document.querySelector(`#span_score[data-playerid='${playerindex}']`).innerHTML = parseInt(document.querySelector(`#span_score[data-playerid='${playerindex}']`).innerHTML, 10) - 1;
         if (document.querySelector(`#span_score[data-playerid='${playerindex}']`).innerHTML === '0') {
           document.querySelector('#span_player1_label').innerHTML = `<span style="font-weight: 600;">There is a winner!</span><br />Congrats! Player ${otherplayer}`;
           document.querySelector('#span_player2_label').innerHTML = `<span style="font-weight: 600;">There is a winner!</span><br />Congrats! Player ${otherplayer}`;
